b1688c7e92db93e87e6b9f8b3748998fc5b8a49e
Nikita Tolstokorov <nikitat@software-kit.ru>
2023-03-13 15:28:37 +0300

    Add new key mappings

--- lua/keymap.lua
+++ lua/keymap.lua
@@ -14,3 +14,11 @@ vim.keymap.set('n', '<leader>p', vim.cmd.Neotree)
 vim.keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle)
 
 vim.keymap.set('n', '<leader>gv', vim.cmd.Git)
+
+vim.keymap.set("x", "<leader>p", "\"_dP")
+
+vim.keymap.set("n", "<leader>y", "\"+y")
+vim.keymap.set("v", "<leader>y", "\"+y")
+vim.keymap.set("n", "<leader>Y", "\"+Y")
+
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
