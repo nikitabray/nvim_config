9ffb14b7682b7641a2b10787f2518bc77f36fadd
Nikita Bray <nikitabray@MacBook-Pro-Nikita.local>
2024-04-11 10:51:28 +0300

    Bind key to show diagnostics float

--- lua/keymap.lua
+++ lua/keymap.lua
@@ -22,4 +22,6 @@ vim.keymap.set("v", "<leader>y", "\"+y")
 vim.keymap.set("n", "<leader>Y", "\"+Y")
 
 vim.keymap.set("n", "<leader>s",
-               [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+	[[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
+vim.keymap.set("n", "<leader>i", vim.diagnostic.open_float)
